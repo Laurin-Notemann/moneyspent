5975db1556fd2e1d4fa7f48ae836246fb8f3527c
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-10-13 16:21:57 +0200

    Initial Commit

---
+++ moneyspent/moneyspentApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyspentApp.swift
+//  moneyspent
+//
+//  Created by Laurin Notemann on 13.10.22.
+//
+
+import SwiftUI
+
+@main
+struct moneyspentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
